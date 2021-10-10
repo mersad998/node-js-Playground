d52a14c4755b3935232a107eb05842ed925c8fd1
mersad <mersad_998@yahoo.com>
2021-10-10 22:53:42 +0330

    feat(index.js): api with html terurn

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
+// this is for return text
+// http.createServer(function(req, res) {
+    
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('hello world!');
+    
+// }).listen(8080, 127.0,0,1);
+
+
+// this is for return html
 http.createServer(function(req, res) {
     
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('hello world!');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myHtmlFile = fs.readFileSync(__dirname + '/index.html');
+    res.end(myHtmlFile);
     
 }).listen(8080, 127.0,0,1);
