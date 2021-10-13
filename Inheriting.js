6e72cd30d25d46f930498aa259a4bb47b4837b0f
mersad <mersad_998@yahoo.com>
2021-10-13 21:42:43 +0330

    feat: something about super in js

--- Inheriting.js
+++ Inheriting.js
@@ -2,6 +2,15 @@ var EventEmitter = require('events');
 var util = require('util');
 
 function customObject (){
+
+    // incase you want to override the context of this customObject to where its wanna inherit from
+    // just can add the line below:
+    //   EventEmitter.call(this);
+    //   or
+    //   EventEmitter.apply(this);
+    // it can work like `super` key in other languages. the prototype chain will stay complete.
+
+
     this.name = 'bla bla bla';
 }
 
