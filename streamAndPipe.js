36e8f62778707172b5f4e275196e740a04db2642
mersad <mersad_998@yahoo.com>
2021-10-18 22:49:54 +0330

    feat: add listener to readable stream

--- streamAndPipe.js
+++ streamAndPipe.js
@@ -7,3 +7,10 @@ var readable = fs.createReadStream(__dirname + '/exampleFile.txt', {
 }); 
 
 var writable = fs.createWriteStream(__dirname + '/exampleFileCopy.txt');
+
+// readable inherited from event emitter in its source code . so it has properties like on in
+// its prototype
+readable.on('data', function (chunk) {
+    console.log('chunk: ', chunk);
+    writable.write(chunk);
+})
