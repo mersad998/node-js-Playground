a46319433cca051955a99b3e794c949182fa2e84
mersad <mersad_998@yahoo.com>
2021-10-22 15:09:55 +0330

    feat: simple api with express

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000 ;
+
+// this will automatically set the content type to text/html
+app.get('/',function(req,res){
+    res.send(
+        '<html><head></head><body><h1>hello world!</h1></body></html>'
+    )
+});
+
+// res.json will automatically convert the js object/array to json with content type application/json
+app.get('/api',function(req,res){
+    res.json({firstName:'my first name', lastName:'my last name'})
+});
+
+app.listen(port);
